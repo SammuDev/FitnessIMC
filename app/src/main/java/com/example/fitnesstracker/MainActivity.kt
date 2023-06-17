9a5e00c48ccbba808ce438e4fe95a6c71de27354
SammuDev <samucas878@gmail.com>
2023-06-17 17:58:16 -0300

    feat: Criando uma nova tela

--- app/src/main/java/com/example/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnImc:LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+        btnImc.setOnClickListener() {
+
+        }
     }
 }
