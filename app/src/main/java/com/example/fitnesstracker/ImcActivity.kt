59542092069e0c1275b45d174662586a09cac010
SammuDev <samucas878@gmail.com>
2023-06-18 10:58:22 -0300

    feat: Alterando o componente 'Button' para uma 'TextView'

--- app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
@@ -1,10 +1,9 @@
 package com.example.fitnesstracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
