94b2bc5f5449172b3c45968477277a2fb600e2af
SammuDev <samucas878@gmail.com>
2023-06-20 08:42:58 -0300

    feat: Conectando os componentes por ID

--- app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
@@ -1,11 +1,26 @@
 package com.example.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
+    private lateinit var weightEditInput: EditText
+    private lateinit var heightEditInput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        weightEditInput = findViewById(R.id.inputEdit_weight)
+        heightEditInput = findViewById(R.id.inputEdit_height)
+
+        val buttonImcCalculator: Button = findViewById(R.id.button_sendImcCalculator)
+        buttonImcCalculator.setOnClickListener {
+            validate()
+        }
+    }
+    private fun validate(): Boolean {
+        if()
     }
 }
