83b45a535f7f60f67e901d2fc9f50cdd14746b6b
SammuDev <samucas878@gmail.com>
2023-06-25 01:20:31 -0300

    feat: Iniciando na criação de uma 'RecyclerView'

--- app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
@@ -1,7 +1,9 @@
 package com.example.fitnesstracker
 
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -38,6 +40,9 @@ class ImcActivity : AppCompatActivity() {
                 .setPositiveButton(android.R.string.ok) { dialog, which -> }
                 .create()
                 .show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
