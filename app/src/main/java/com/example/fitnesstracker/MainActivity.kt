a63a8ee00b49b3db8d640f234c0e219afc7ac88d
SammuDev <samucas878@gmail.com>
2023-06-25 16:41:06 -0300

    feat: Criando as classes da 'RecyclerView'

--- app/src/main/java/com/example/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -1,21 +1,48 @@
 package com.example.fitnesstracker
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnImc: LinearLayout
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
-        btnImc.setOnClickListener() {
-            val intent = Intent(this, ImcActivity::class.java)
-            startActivity(intent)
+        val mainAdapter = MainAdapter()
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = mainAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+//        btnImc = findViewById(R.id.btn_imc)
+//        btnImc.setOnClickListener() {
+//            val intent = Intent(this, ImcActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            TODO("Not yet implemented")
         }
     }
+
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
