7243d53aceab589da700e8bee7d77335267f4694
SammuDev <samucas878@gmail.com>
2023-06-26 02:23:41 -0300

    feat: Dando corpo aos componentes da RecyclerView, e renderizando 5 (cinco) layouts, cada um com botão e texto

--- app/src/main/java/com/example/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -1,48 +1,58 @@
 package com.example.fitnesstracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnImc: LinearLayout
-
+    lateinit var buttonImc: LinearLayout
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainAdapter = MainAdapter()
-
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.adapter = mainAdapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-//        btnImc = findViewById(R.id.btn_imc)
-//        btnImc.setOnClickListener() {
-//            val intent = Intent(this, ImcActivity::class.java)
-//            startActivity(intent)
-//        }
-    }
-
-    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-            TODO("Not yet implemented")
+        buttonImc = findViewById(R.id.button_imc)
+        buttonImc.setOnClickListener() {
+            val intent = Intent(this, ImcActivity::class.java)
+            startActivity(intent)
         }
 
-        override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+        val myAdapter = MyAdapter()
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = myAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private inner class MyAdapter : RecyclerView.Adapter<MainViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-            TODO("Not yet implemented")
+            val item = "Item ${position + 1}"
+            holder.bind(item)
+        }
+
+        override fun getItemCount(): Int {
+            return 5
         }
     }
 
-    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val itemTextView: TextView = view.findViewById(R.id.textView_item)
+
+        fun bind(item: String) {
+            itemTextView.text = item
+        }
+    }
 }
