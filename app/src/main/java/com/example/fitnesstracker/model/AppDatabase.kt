df09880bf7ec181d8064c758473b1d36b3864da1
SammuDev <samucas878@gmail.com>
2023-07-02 04:48:52 -0300

    feat: Criando classe que representa o banco de dados do aplicativo e que contém métodos para acessar os DAOs correspondentes.

--- app/src/main/java/com/example/fitnesstracker/model/AppDatabase.kt
+++ app/src/main/java/com/example/fitnesstracker/model/AppDatabase.kt
@@ -1,7 +1,30 @@
 package com.example.fitnesstracker.model
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Calculate::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun calculateDao(): CalculateDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "fitness_tracker"
+                    ).build()
+                }
+                INSTANCE as AppDatabase
+            } else {
+                INSTANCE as AppDatabase
+            }
+        }
+    }
 }
